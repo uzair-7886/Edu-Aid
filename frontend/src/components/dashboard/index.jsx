12c0a21d436109c4ff241e1faa38c0aa7f92fcca
alpha-uzair <jerry.khan7886@gmail.com>
2023-05-06 16:58:50 +0500

    auth at dashboard

--- frontend/src/components/dashboard/index.jsx
+++ frontend/src/components/dashboard/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import jwt_decode from 'jwt-decode'
 
 export default function Dashboard() {
+
+    const token=localStorage.getItem("token");
+    const decoded=jwt_decode(token);
+    console.log(decoded)
     const handleLogout=()=>{
         localStorage.removeItem("token");
         window.location.reload();
